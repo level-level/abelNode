78143b0e228c8707eda9faba14243492b0ef4e2e
ugur22 <ugurertas9@gmail.com>
2018-04-13 00:14:01 +0200

    axe-webdriverjs working showing resilst in console

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-node-ejs
+Abel - Accessibility checker
 ========
 
-Using EJS to template a Node application.
+This project is build on the Express framework in NodeJS.
+
+This webapp uses the ![axe-webdriverjs](https://github.com/dequelabs/axe-webdriverjs) to display accessibility issues in your html elements.
+
+It also need to use selenium-webdriver to start a browser that to test your website.
+
+Use npm install to install all necessary packages
+
+```
+cd abelNode
+npm install
+```
+
+here below is a sample code that shows how to test a url:
+
+```
+var driver = new WebDriver.Builder()
+		.forBrowser('firefox')
+		.build();
+
+
+	driver
+		.get('https://level-level.com')
+		.then(function () {
+			AxeBuilder(driver)
+				.analyze(function (results) {
+					driver.quit();
+					console.log(results);
+				});
+		});
+```
+
+
+
--- server.js
+++ server.js
@@ -1,19 +1,49 @@
 // load the things we need
 var express = require('express');
 var app = express();
+var AxeBuilder = require('axe-webdriverjs');
+var WebDriver = require('selenium-webdriver');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+
+// Axe web test
+
+
 // use res.render to load up an ejs view file
 
 // index page 
-app.get('/', function(req, res) {
-	res.render('pages/index');
+app.get('/', function (req, res) {
+	var header = "Violations";
+	var description = "des";
+	var violations = [];
+
+	var driver = new WebDriver.Builder()
+		.forBrowser('firefox')
+		.build();
+
+
+	driver
+		.get('https://level-level.com')
+		.then(function () {
+			AxeBuilder(driver)
+				.analyze(function (results) {
+					violations.push(results);
+					driver.quit();
+					console.log(violations[0]['violations'][0]);
+				});
+		});
+
+	res.render('pages/index', {
+		header: header
+	});
 });
 
+
+
 // about page 
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
 	res.render('pages/about');
 });
 
