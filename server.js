1518ea4e3db1b4d8f2fba3568ed4f91a8f797a1e
ugur22 <ugurertas9@gmail.com>
2018-04-17 11:12:50 +0200

    Test result Axe displayed on page

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // load the things we need
 var express = require('express');
 var app = express();
+var http = require('http');
+let io = require('socket.io');
 var AxeBuilder = require('axe-webdriverjs');
 var WebDriver = require('selenium-webdriver');
 const puppeteer = require('puppeteer');
@@ -11,27 +13,39 @@ var urlencoderParser = bodyParser.urlencoded({ extended: true });
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+// Set the port number
+let port = 8080;
+
 app.post('/', urlencoderParser, function (req, res) {
 	var header = "Test your website";
-	console.log(req.body.url);
-
 	url = req.body.url;
+	var finalUrl;
 
 	var violations = [];
+	var result;
 	var driver = new WebDriver.Builder()
 		.forBrowser('firefox')
 		.setFirefoxOptions(new firefox.Options().headless())
 		.build();
 
+	if (url.indexOf("http://") == 0 || url.indexOf("https://") == 0) {
+		finalUrl = url;
+	} else {
+		finalUrl = "https://" + url;
+	}
 
+	console.log(finalUrl);
+	
 	driver
-		.get("https://"+url)
+		.get(finalUrl)
 		.then(function () {
 			AxeBuilder(driver)
 				.analyze(function (results) {
 					violations.push(results);
 					driver.quit();
-					console.log(violations[0]['violations'][0]);
+					console.log(violations[0]['violations']);
+					result = violations[0]['violations'];
+					io.emit('data', result);
 				});
 		});
 
@@ -48,6 +62,7 @@ app.get('/', function (req, res) {
 	res.render('pages/index', {
 		header: header
 	});
+
 });
 
 
@@ -57,5 +72,22 @@ app.get('/about', function (req, res) {
 	res.render('pages/about');
 });
 
-app.listen(8080);
-console.log('8080 is the magic port');
+
+
+const server = http.createServer(app).listen(port, function (req, res) {
+	console.log('LISTENING ON PORT ' + port);
+});
+
+
+// Set up socket.io to 'listen'
+io = io.listen(server);
+
+// Display a conection message
+io.on('connection', function (socket) {
+	console.log('SOCKET.IO CONNECTED');
+
+	// Display a disconnection message
+	socket.on('disconnect', function () {
+		console.log('SOCKET.IO DISCONNECTED');
+	})
+});
